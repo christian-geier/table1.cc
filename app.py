2f0e475132bf91367f64977b21c2017fd8606ed7
Christian Geier <7210438+christian-geier@users.noreply.github.com>
2019-12-10 22:28:50 -0500

    now using wtforms

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, escape, request, render_template, url_for
 
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "very_secret"
+
+
+class PasteForm(FlaskForm):
+    excel_data = TextAreaField(u'Text', id="pastein", widget=TextArea(), validators=[DataRequired()])
+    submit = SubmitField('Load Data Set')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     if request.method=='POST':
-        value = request.form['value']
-        print("The value is:" + str(value))
+        body = request.form['excel_data']
+        print("The value is:" + str(body))
 
-    return render_template('index.html')
+
+    paste_form = PasteForm()
+    return render_template('index.html', form=paste_form)
