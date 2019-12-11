771da509f9eff4c3c449f310ffe4b28b9cba2d59
Christian Geier <7210438+christian-geier@users.noreply.github.com>
2019-12-10 22:49:50 -0500

    first prototype !

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 from wtforms.widgets import TextArea
 
+import pandas as pd
+from pandas import read_csv
+
+from io import StringIO
+
+from tableone import TableOne
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "very_secret"
 
@@ -17,9 +24,12 @@ class PasteForm(FlaskForm):
 def index():
 
     if request.method=='POST':
-        body = request.form['excel_data']
-        print("The value is:" + str(body))
-
+        raw_data = request.form['excel_data']
+        raw_string = StringIO(raw_data)
+        df = read_csv(raw_string, sep='\t')
+        my_table = TableOne(df, groupby='provider')
+        my_table_html = my_table.tabulate(tablefmt="html")
+        return my_table_html
 
     paste_form = PasteForm()
     return render_template('index.html', form=paste_form)
