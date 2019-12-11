e0b0169c30b93d05f472023b52ff7044160750fd
Christian Geier <7210438+christian-geier@users.noreply.github.com>
2019-12-10 19:08:13 -0500

    now displaying submit button

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, escape, request, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+
+    if request.method=='POST':
+        value = request.form['value']
+        print("The value is:" + str(value))
+
     return render_template('index.html')
