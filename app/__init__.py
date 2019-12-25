d371f52bde85d5675daa7b37954b0ecd044f09eb
Col.Slade <Christian Geier>
2019-12-25 11:22:09 -0500

    adds whitenoise for serving static of future blog

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from flask_bootstrap import Bootstrap
 
 from flask import Flask
 
+from whitenoise import WhiteNoise
+
 from config import Config
 
 app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='app/static/', index_file=True)
 app.config.from_object(Config)
 
 bootstrap = Bootstrap(app)
